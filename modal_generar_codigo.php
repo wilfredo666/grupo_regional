57f72120d7426f09659bb3b6e7c4d66d5578753f
wilfredo666 <wilfredosaez@hotmail.com>
2019-01-09 08:30:36 -0400

    09/01/19

---
+++ modal_generar_codigo.php
@@ -0,0 +1,38 @@
+<div class="modal fade bd-example-modal-lg" id="generar">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Indique los elementos para generar el codigo de proyecto</h5>
+                <button class="close" type="button" data-dismiss="modal" arial-label="close">
+                    <span aria-hidden="true">x</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-group">
+                        <label for="" class="col-form-label">Area</label>
+                        <select name="" id="" class="form-control">
+                            <option value="">--</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="col-form-label">Ciudad</label>
+                        <select name="" id="" class="form-control">
+                            <option value="">--</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="col-form-label">Cliente</label>
+                        <select name="" id="" class="form-control">
+                            <option value="">--</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="col-form-label">Codigo</label>
+                        <input type="text" readonly class="form-control">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
